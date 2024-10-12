e101c4128bc27721cc5ac999a805a06eebb31a0b
matsushita2002 <bsk-ryo-m@docomo.ne.jp>
2024-10-12 18:40:19 +0900

    Update script.js
    set Kurea

--- js/script.js
+++ js/script.js
@@ -1,11 +1,11 @@
 'use strict';
 
-// window.alert('こんにちは');
-
 const name = window.prompt("姓名を漢字で入力してください");
 
 if (name === '和田渚') {
-    window.alert('なぎとくーちゃんこんにちは！！');
+    window.alert('なぎこんにちは！！');
+}else if (name === null) {
+    window.alert('くーちゃんこんにちは！！');
 }else {
     window.alert(name+'さんこんにちは！！');
 }
